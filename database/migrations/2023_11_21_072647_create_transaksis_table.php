1e8045bfb574e2f901c3dc1ef4eb1847ec374651
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-12-23 00:08:41 +0700

    feat: add table migration

---
+++ database/migrations/2023_11_21_072647_create_transaksis_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('total_price', 15, 2);
+            $table->string('image_url');
+            $table->string('report')->nullable();
+            $table->foreignId('plan_id')->constrained('plans');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('status_id')->constrained('statuses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
