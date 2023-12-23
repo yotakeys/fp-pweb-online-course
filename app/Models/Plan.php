6cb0d1b7b40e9314699d1414c045d6024433970e
Muhammad Rafif Tri Risqullah <zeonkunix@gmail.com>
2023-12-24 06:41:54 +0700

    feat: add model plan

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -4,8 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Plan extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+    ];
+
+
+    /**
+     * Get all of the courses for the Plan
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function courses(): HasMany
+    {
+        return $this->hasMany(Course::class);
+    }
+
+    public function transaksi(): HasMany
+    {
+        return $this->hasMany(Transaksi::class);
+    }
 }
